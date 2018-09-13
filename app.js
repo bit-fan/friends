1b1084c4b9a4b874d0a237dc68715cf713b67c43
asd <taur57@hotmail.com>
2018-09-13 11:10:22 +0800

    update home path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/home', indexRouter);
 app.use('/api', usersRouter);
 
 // catch 404 and forward to error handler
