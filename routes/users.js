0b56b6682351bd643fcbecfb5291f92a959de290
asd <taur57@hotmail.com>
2018-09-13 10:58:59 +0800

    minor enhancement for dealing with multi user in future.

--- routes/users.js
+++ routes/users.js
@@ -3,43 +3,44 @@ var router = express.Router();
 const Friend = require('../server/friendLib');
 
 const friend = new Friend();
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
 
+function getCurrentFriend(req) {
+  // for now, we only use one friend setup.
+  // in futute can use different data for different req
+  return friend;
+}
 router.post('/addFriend', function (req, res, next) {
-  friend.add(req.body.friends[0], req.body.friends[1])
+  getCurrentFriend(req).add(req.body.friends[0], req.body.friends[1])
     .then(data => {
       res.send(data);
     });
 })
 router.post('/getFriends', function (req, res, next) {
-  friend.getFriends(req.body.email)
+  getCurrentFriend(req).getFriends(req.body.email)
     .then(data => {
       res.send(data);
     });
 })
 router.post('/commonFriend', function (req, res, next) {
-  friend.commonFriend(req.body.friends[0], req.body.friends[1])
+  getCurrentFriend(req).commonFriend(req.body.friends[0], req.body.friends[1])
     .then(data => {
       res.send(data);
     });
 })
 router.post('/subscribeUpdate', function (req, res, next) {
-  friend.subscribe(req.body.requestor, req.body.target)
+  getCurrentFriend(req).subscribe(req.body.requestor, req.body.target)
     .then(data => {
       res.send(data);
     });
 })
 router.post('/block', function (req, res, next) {
-  friend.block(req.body.requestor, req.body.target)
+  getCurrentFriend(req).block(req.body.requestor, req.body.target)
     .then(data => {
       res.send(data);
     });
 })
 router.post('/retrieve', function (req, res, next) {
-  friend.retrieve(req.body.sender, req.body.text)
+  getCurrentFriend(req).retrieve(req.body.sender, req.body.text)
     .then(data => {
       res.send(data);
     });
